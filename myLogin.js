e20137d9c67bda6215ec63612d215f186a825f45
First Last <email>
2024-03-03 16:34:48 -0700

    transition from login to play

--- myLogin.js
+++ myLogin.js
@@ -1,5 +1,11 @@
 function login() {
-  const nameEl = document.querySelector("#name");
-  localStorage.setItem("userName", nameEl.value);
+  const nameEl_1 = document.querySelector("#name_1");
+  localStorage.setItem("user_1", nameEl_1.value);
+  const nameEl_2 = document.querySelector("#name_2");
+  localStorage.setItem("user_1", nameEl_2.value);
+  const nameEl_3 = document.querySelector("#name_3");
+  localStorage.setItem("user_1", nameEl_3.value);
+  const nameEl_4 = document.querySelector("#name_4");
+  localStorage.setItem("user_1", nameEl_4.value);
   window.location.href = "play.html";
 }
